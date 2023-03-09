e1f4bf4da55f0e768a3b2642b758495bb963a665
Nikolay <nikolay_play2000@mail.ru>
2023-03-10 00:09:57 +0300

    add

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <q-btn @click.prevent="$router.push({ name: 'about' })">about</q-btn>
     <q-page-container>
       <router-view />
     </q-page-container>
