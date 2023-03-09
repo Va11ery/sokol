6d50f64cb97c4f19114315e7eb01b097a4e26fec
Va11ery <109084396+Va11ery@users.noreply.github.com>
2023-03-09 21:01:07 +0000

    Deploying to gh-pages from @ Va11ery/sokol@dd7586234f221f3b7d5748f200da91694024140a 🚀

---
+++ assets/ClientsCompany.f355e1f3.js
@@ -0,0 +1,1 @@
+import{bw as e,a6 as t,ay as s,az as a}from"./index.7e642d86.js";const c={},o={class:"fill-height container"},n=a("div",{class:"title-3vw ptb-100"},"\u041A\u043B\u0438\u0435\u043D\u0442\u044B",-1),r=[n];function _(i,l){return t(),s("div",o,r)}var f=e(c,[["render",_]]);export{f as default};
