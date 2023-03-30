7441c13ece892879731e10e3103738347b5c97f3
Va11ery <109084396+Va11ery@users.noreply.github.com>
2023-03-30 21:14:47 +0000

    Deploying to gh-pages from @ Va11ery/sokol@9220d2b5de3c01b0b8ef1354d1bd261f1385c20e 🚀

---
+++ assets/TeamsCompany.4fcfda3d.js
@@ -0,0 +1,1 @@
+import{_ as e,Q as t,W as s,a1 as a}from"./index.41100035.js";const c={},o={class:"fill-height container"},n=a("div",{class:"title-3vw ptb-100 text-white"},"\u041A\u043E\u043C\u0430\u043D\u0434\u0430",-1),_=[n];function r(i,d){return t(),s("div",o,_)}var f=e(c,[["render",r]]);export{f as default};
